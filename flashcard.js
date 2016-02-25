535c92a81bb752c0768f5fb359b73046ca0398a7
Max Kaye <maxkaye@Maxs-MacBook-Pro.local>
2016-02-25 14:19:41 -0500

    Project Commit

--- flashcard.js
+++ flashcard.js
@@ -1,7 +1,12 @@
 
-function logCharacters(chinese, english, pinyin) {
-  console.log('a[' + english + '] = ' + chinese);
-}
-["你好", "早上好", "吃饭"].forEach(function(word){
-  console.log(word);
+var myCharacters = [
+ {english: "hello", chinese: "你好", pronounce: "ni3hao3"}
+];
+myCharacters.forEach(function(character){
+ var myText = document.createElement("INPUT");
+ myText.value = character.chinese;
+ var translate = document.createElement("BUTTON");
+ button.addEventListener("click", function(){
+   alert(character.english);
+ });
 });
